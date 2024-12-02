ed594ad9e97fb637c41081a4756867a16619bf66
thisIsMyName1309 <marian@tryincant.com>
2024-12-02 11:27:55 +0200

    feat: admin dashboard

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -23,12 +23,14 @@ type Attempt = {
 
 const AdminPage = () => {
   const [attempts, setAttempts] = useState<Attempt[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchAttempts = async () => {
       const res = await fetch('/api/auth/log-attempts');
       const data = await res.json();
       setAttempts(data);
+      setLoading(false);
     };
 
     fetchAttempts();
@@ -42,31 +44,35 @@ const AdminPage = () => {
         </CardHeader>
 
         <CardContent>
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead>Email</TableHead>
-                <TableHead>IP Address</TableHead>
-                <TableHead>Success</TableHead>
-                <TableHead>Time</TableHead>
-              </TableRow>
-            </TableHeader>
-
-            <TableBody>
-              {attempts.map((attempt) => (
-                <TableRow key={attempt.id}>
-                  <TableCell>{attempt.email}</TableCell>
-                  <TableCell>{attempt.ipAddress}</TableCell>
-                  <TableCell>
-                    <Badge variant={attempt.success ? 'default' : 'destructive'}>
-                      {attempt.success ? 'Yes' : 'No'}
-                    </Badge>
-                  </TableCell>
-                  <TableCell>{new Date(attempt.createdAt).toLocaleString()}</TableCell>
+          {loading ? (
+            <div className="flex justify-center">Loading...</div>
+          ) : (
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Email</TableHead>
+                  <TableHead>IP Address</TableHead>
+                  <TableHead>Success</TableHead>
+                  <TableHead>Time</TableHead>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
+              </TableHeader>
+
+              <TableBody>
+                {attempts.map((attempt) => (
+                  <TableRow key={attempt.id}>
+                    <TableCell>{attempt.email}</TableCell>
+                    <TableCell>{attempt.ipAddress}</TableCell>
+                    <TableCell>
+                      <Badge variant={attempt.success ? 'default' : 'destructive'}>
+                        {attempt.success ? 'Yes' : 'No'}
+                      </Badge>
+                    </TableCell>
+                    <TableCell>{new Date(attempt.createdAt).toLocaleString()}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          )}
         </CardContent>
       </Card>
     </div>
